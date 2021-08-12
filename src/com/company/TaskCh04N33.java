7d792be859473d8a359d7886339a120903838ff0
Ilyas Akhmadiev <ilyas.akhmadiev@gmail.com>
2021-08-12 11:24:26 +0300

    add TaskCh04N33

--- src/com/company/TaskCh04N33.java
+++ src/com/company/TaskCh04N33.java
@@ -1,7 +1,17 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class TaskCh04N33 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число");
+        int a = sc.nextInt();
+        if ((a%2)==0) {
+            System.out.println(a+" - Четное число");
+        }
+        else {
+            System.out.println(a+" - Нечетное число");
+        }
     }
 }
