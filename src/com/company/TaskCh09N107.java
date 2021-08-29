d98755a4d64e16819191018166c37368360d80cc
Ilyas Akhmadiev <ilyas.akhmadiev@gmail.com>
2021-08-29 16:29:31 +0300

    Работа с символами и строками

--- src/com/company/TaskCh09N107.java
+++ src/com/company/TaskCh09N107.java
@@ -9,12 +9,13 @@ public class TaskCh09N107 {
         int a = word.indexOf('a');
         int o = word.lastIndexOf('o');
         if (a>=0 && o>=0) {
-        int find1 = word.indexOf("a");
-        int find2 = word.lastIndexOf("o");
-        String word2 = word.replaceFirst(String.valueOf(word.charAt(find2)), "a");
-        String word3 = word2.replaceFirst(String.valueOf(word2.charAt(word2.indexOf("a"))), "o");
-            System.out.println(word3);
-        }
+        String word2 = word.replaceFirst(String.valueOf(word.charAt(word.indexOf('a'))), "o");
+        int find = word2.lastIndexOf('o');
+        String sub = word2.substring(find, word.length());
+            String word3 = sub.replaceFirst(String.valueOf(sub.charAt(sub.lastIndexOf('o'))), "a");
+            String sub2 = word2.substring(0, find);
+            System.out.println(sub2+word3);
+        }   
         else {
             System.out.println("В слове нет букв 'a' и 'o'");
         }
