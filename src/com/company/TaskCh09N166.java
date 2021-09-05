916580b76e1a4a89cb9cc24da0110887125d249d
Ilyas Akhmadiev <ilyas.akhmadiev@gmail.com>
2021-09-05 11:36:51 +0300

    String.split

--- src/com/company/TaskCh09N166.java
+++ src/com/company/TaskCh09N166.java
@@ -1,2 +1,20 @@
-package com.company;public class TaskCh09N166 {
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class TaskCh09N166 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String text = scan.nextLine();
+        String[] splitResult = text.split(" ");
+        System.out.println(Arrays.toString(splitResult));
+        String start = splitResult[0];
+        String last = splitResult[splitResult.length - 1];
+        splitResult[splitResult.length - 1] = splitResult[0];
+        splitResult[0] = last;
+        text = Arrays.stream(splitResult).collect(Collectors.joining(" "));
+        System.out.println(text);
+    }
 }
